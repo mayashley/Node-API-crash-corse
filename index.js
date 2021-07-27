ad3c5398b2f81531e75b17bb151973bdd0bc629f
mayashley <54293844+mayashley@users.noreply.github.com>
2021-07-27 15:20:04 -0400

    practice with person name and list of people in the module

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello From Node.js");
+const Person = require('./person');
+
+const Person1 = new Person ('Ashley', 28);
+const Person2 = new Person (' Raymond', 29);
+
+Person2.greeting();
