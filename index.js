45f1052d2a38434b0f20c355c5733a22f48fae5d
mayashley <54293844+mayashley@users.noreply.github.com>
2021-07-27 16:03:16 -0400

    formatted

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Person = require('./person');
+const Person = require("./person");
 
-const Person1 = new Person ('Ashley', 28);
-const Person2 = new Person (' Raymond', 29);
+const Person1 = new Person("Ashley", 28);
+const Person2 = new Person(" Raymond", 29);
 
 Person2.greeting();
--- person.js
+++ person.js
@@ -1,10 +1,10 @@
 class Person {
-constructor (name, age) {
-this.name = name;
-this.age = age;
-}
-greeting() {
-    console.log (`My name is ${this.name} and I am ${this.age}`)
-}
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greeting() {
+    console.log(`My name is ${this.name} and I am ${this.age}`);
+  }
 }
 module.exports = Person;
